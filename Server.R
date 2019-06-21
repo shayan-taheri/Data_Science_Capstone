a5aecc4923e2f3bac0c3b3385f67d2bd8c3dd601
Shayan (Sean) Taheri <shayan.taheri@knights.ucf.edu>
2019-06-21 16:20:40 -0400

    Add files via upload

---
+++ Server.R
@@ -0,0 +1,17 @@
+# Author: Shayan (Sean) Taheri
+# Final Project Sever Application
+
+library(shiny)
+library(datasets)
+library(dplyr)
+library(knitr)
+
+source("C:/Users/shaya/Desktop/Data_Science_Capstone/FP_Functions.R")
+
+# Server Version Definition for Running Prediction Algorithm
+shinyServer(function(input, output) {
+        predict <- reactive({
+                input$textinput %>% nextword()
+        })
+        output$textoutput <- renderText(predict())
+})
